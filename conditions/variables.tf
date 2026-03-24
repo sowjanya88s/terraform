9903bfb78ae6e7afe60bbc8a5e812d6599487db8
sowjanya <devopssowjanya88s@gmail.com>
2026-03-24 16:23:34 +0530

    new

---
+++ conditions/variables.tf
@@ -0,0 +1,56 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+}
+
+variable "environment" {
+    type = string
+    default = "prod"
+}
+
+
+variable "tags" {
+    type = map
+    default = {
+      Name = "terraform"
+      Env = "dev"
+      terraform = "true"
+    }
+}
+
+variable "sg_name" {
+    type = string
+    default = "terraform-sg"
+}
+
+variable "sg_description" {
+    type = string
+    default = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+variable "from_port" {
+    type = number
+    default = 0
+}
+
+variable "to_port" {
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_protocol" {
+    type = string
+    default = "-1"
+}
+
+variable "sg_tags" {
+    type = map
+    default = {
+      Name = "terraform-sg"  
+    }
+}
