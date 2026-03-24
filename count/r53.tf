0dc6a6ac031339ed5307d380ea69aa9126a27c05
sowjanya <devopssowjanya88s@gmail.com>
2026-03-24 17:17:04 +0530

    new

--- count/r53.tf
+++ count/r53.tf
@@ -1,7 +1,8 @@
 resource "aws_route53_record" "www" {
+    count = 10
   zone_id = var.zone_id
-  name    = var.zone_name
+  name    = "${var.instances[count.index]}.${var.zone_name}"
   type    = "A"
   ttl     = 1
-  records = "${var.instances[count.index]}"."${var.zone_name}"
+  records = [aws_instance.my_instance.[count.index].private_ip]
 }
