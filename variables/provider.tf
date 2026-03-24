d1f1a2cba223156626762eb989450035e6e02c80
sowjanya <devopssowjanya88s@gmail.com>
2026-03-24 16:11:02 +0530

    new

---
+++ variables/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.37.0" # aws provider version
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  regoin = "us-east-1"
+}
---
+++ variables/variables.tf
@@ -0,0 +1,55 @@
+variable "ami_id" {
+    type = string
+    default = "ami-0220d79f3f480ecf5"
+}
+
+variable "instance_type" {
+    type = string
+    default = "t3.micro"
+}
+
+variable "tags" {
+    type = map
+    default = {
+      Name = "terraform"
+      Env = "dev"
+      terraform = "true"
+    }
+}
+
+variable "sg_name" {
+    type = string
+    default = "terraform-sg"
+}
+
+variable "sg_description" {
+    type = string
+    default = "Allow TLS inbound traffic and all outbound traffic"
+}
+
+variable "from_port" {
+    type = number
+    default = 0
+}
+
+variable "to_port" {
+    type = number
+    default = 0
+}
+
+variable "cidr_blocks" {
+    type = list
+    default = ["0.0.0.0/0"]
+}
+
+variable "sg_protocol" {
+    type = string
+    default = "-1"
+}
+
+variable "sg_tags" {
+    type = map
+    default = {
+      Name = "terraform-sg"  
+    }
+}
