e8e2584ca6b1c6032ac8d1653085cde93af49326
sowjanya <devopssowjanya88s@gmail.com>
2026-03-25 21:14:44 +0530

    new

---
+++ for-each/r53.tf
@@ -0,0 +1,8 @@
+resource "aws_route53_record" "www" {
+  for_each = aws_instance.my_instance
+  zone_id = var.zone_id
+  name    = "${each.key}.${var.zone_name}"
+  type    = "A"
+  ttl     = 1
+  records = [each.key.private_ip]
+}
