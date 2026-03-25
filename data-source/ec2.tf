3515c42a87e25c1dda0ab08dabda819ef2ed67d0
sowjanya <devopssowjanya88s@gmail.com>
2026-03-25 22:06:09 +0530

    new

--- data-source/ec2.tf
+++ data-source/ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my_instance" {
   ami           = data.aws_ami.example.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  
   tags = {
     Name = "terraform"
     Env = "dev"
