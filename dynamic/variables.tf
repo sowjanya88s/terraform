9f92ce6bed4eb2d41f2013253107610b3f113d93
sowjanya <devopssowjanya88s@gmail.com>
2026-03-25 21:39:24 +0530

    new

--- dynamic/variables.tf
+++ dynamic/variables.tf
@@ -14,3 +14,20 @@ variable "ingress_rules" {
 },
     ]
 }
+
+variable "zone_id" {
+    type = string
+    default = "Z082049010RMR2FN1A4VI"
+}
+variable "zone_name" {
+    type = string
+    default = "sowjanya.fun"
+}
+   variable "instances" {
+    type = map
+    default = {
+        mongodb = "t3.micro"
+        catalogue = "t3.micro"
+        redis = "t3.small"
+    }
+} 
