a2d4b0a7eabebeb9f42f0efdc87686c0ab0eb1f0
sowjanya <devopssowjanya88s@gmail.com>
2026-03-27 16:41:34 +0530

    new

---
+++ locals/ec2.tf
@@ -0,0 +1,26 @@
+resource "aws_instance" "my_instance" {
+  ami           = "ami-0220d79f3f480ecf5"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
+  tags = var.ec2_name
+}
+
+resource "aws_security_group" "my_sg" {
+  name        = "terraform-vpc"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
---
+++ locals/variables.tf
@@ -0,0 +1,17 @@
+variable "env" {
+    default = "dev"
+}
+
+variable "ec2_tags" {
+    type = string
+    default = "locals-instance"
+}
+
+variable "Name" {
+    type = string
+    default = "locals"
+}
+ variable "ec2_name" {
+    type = string
+    default = "${var.env}-${var.Name}"
+ }
