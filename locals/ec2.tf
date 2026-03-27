7f76c79749e9ce56f37302b8167a85ccf250bbb7
sowjanya <devopssowjanya88s@gmail.com>
2026-03-27 16:46:06 +0530

    new

--- locals/ec2.tf
+++ locals/ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "my_instance" {
   ami           = "ami-0220d79f3f480ecf5"
-  instance_type = "t3.micro"
+  instance_type = local.instance_type
   vpc_security_group_ids = [aws_security_group.my_sg.id]
-  tags = var.ec2_name
+  tags = local.ec2_name
 }
 
 resource "aws_security_group" "my_sg" {
--- locals/locals.tf
+++ locals/locals.tf
@@ -1,4 +1,5 @@
 locals {
     ec2_tags = "${var.env}-${var.Name}"
+    instance_type = "t3.micro"
 
 }
