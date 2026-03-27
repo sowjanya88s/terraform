2cbcba7b8a30171e82d157dc4efce1993d0d9558
sowjanya <devopssowjanya88s@gmail.com>
2026-03-27 17:00:42 +0530

    new

--- locals/locals.tf
+++ locals/locals.tf
@@ -6,6 +6,6 @@ locals {
         Terraform = "true"
         Environment = "dev"
     }
-ec2_final_tags = "merge(var.common_tags, var.ec2_tags)"
+ec2_final_tags = merge(local.common_tags, var.ec2_tags)
 
 }
