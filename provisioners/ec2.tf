4fa70868b6487c1288030a461bb0b63400e3e3a0
sowjanya <devopssowjanya88s@gmail.com>
2026-03-27 17:26:29 +0530

    new

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -7,11 +7,13 @@ resource "aws_instance" "my_instance" {
     Env = "dev"
   }
  provisioner "local-exec" {
-  interpreter = ["bash", "-c"]
+  # interpreter = ["bash", "-c"]
   command     = "echo ${self.public_ip} > inventory.ini"
 }
 
-
+provisioner "local-exec" {
+  command = "exit 1"
+}
 
 }
 
