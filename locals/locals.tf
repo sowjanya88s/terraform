46f2901e1e540f09243c7170c17dc569016affbf
sowjanya <devopssowjanya88s@gmail.com>
2026-03-27 16:57:48 +0530

    new

--- locals/locals.tf
+++ locals/locals.tf
@@ -1,11 +1,11 @@
 locals {
-    instance_name = "${var.Name}-${var.env}"
+    instance_name = "${var.name}-${var.env}"
     instance_type = "t3.micro"
     common_tags = {
         Project = "roboshop"
         Terraform = "true"
         Environment = "dev"
     }
-ec2_final_tags = "merge(var.common_tags,var.ec2_tags)"
+ec2_final_tags = "merge(var.common_tags, var.ec2_tags)"
 
 }
